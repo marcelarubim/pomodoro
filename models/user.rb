b7b8f82f9a5a0096a775a123e9046dd87b715dfb
Marcela Rubim <marcelarubimv@gmail.com>
2017-10-03 16:57:21 +0200

    Username and email validations improved

--- models/user.rb
+++ models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   include BCrypt
   validates_uniqueness_of :email, :username, case_sensitive: false
   validates_presence_of :email, :username, :password_hash
+  validates :username, length: { minimum: 3 }
+  validate :validate_email
+  validate :validate_username
   enum role: %w[admin free premium]
   has_many :sessions
 
@@ -38,5 +41,17 @@ class User < ActiveRecord::Base
     elsif role == 'premium'
       %w[view_session add_session view_stats]
     end
+  end
+
+  private
+
+  def validate_email
+    return if email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    errors[:email] << 'is not an email'
+  end
+
+  def validate_username
+    return if username =~ /\A\w+\z/ || username == email
+    errors[:username] << 'invalid characteres'
   end
 end
